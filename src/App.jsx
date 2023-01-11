106d07ddf652edb6a539f56eba8ffca2e8291569
[헬스케어]선인장 <sij919@douzone.com>
2023-01-11 14:16:31 +0900

    전체 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 // 이미지 코드 추가
-import img01 from '../public/assets/images/png/img01.png';
-import img02 from '../public/assets/images/gif/img02.gif';
-import img03 from '../public/assets/images/jpg/img03.jpg';
-import img04 from '../public/assets/images/png/img04.png';
-import img05 from '../public/assets/images/gif/img05.gif';
-import img06 from '../public/assets/images/jpg/img06.jpg';
-import img07 from '../public/assets/images/png/img07.png';
-import img08 from '../public/assets/images/png/img08.png';
+import img01 from './assets/images/png/img01.png';
+import img02 from './assets/images/gif/img02.gif';
+import img03 from './assets/images/jpg/img03.jpg';
+import img04 from './assets/images/png/img04.png';
+import img05 from './assets/images/gif/img05.gif';
+import img06 from './assets/images/jpg/img06.jpg';
+import img07 from './assets/images/png/img07.png';
+import img08 from './assets/images/png/img08.png';
 
 class App extends React.Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const Imageminplugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
+  /* entry: 다른 모듈을 사용하고 있는 최상위 자바스크립트 파일 위치 */
   entry: path.join(__dirname, 'src', 'index.js'),
   mode: 'development',
+  /* output: 내보내기할 경로 지정 */
   output: {
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
+      /* js, jsx - babel-loader 설정 */
       {
         test: /\.?(js|jsx)$/,
         exclude: /node_modules/,
@@ -24,13 +28,31 @@ module.exports = {
           extensions: ['', '.js', '.jsx'],
         },
       },
+      /* sa[c]ss - style-loader(css-loader, sass-loader) 설정 */
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       },
+      /* 이미지 파일(git, png, jp[e]g, svg): file-loader */
       {
         test: /\.(png|jp(e*)g|svg|gif)$/,
         use: ['file-loader?name=[path][name].[ext]'],
+        /* file-loader는 이미지 파일의 이름을 만들고 폴더를 이동시킴 */
+        /* [path][name].[ext]: 'dist/이미지 파일 경로/이미지.확장자' */
       },
     ],
   },
@@ -38,6 +60,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html'),
     }),
+    new CleanWebpackPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'development'
@@ -47,7 +70,6 @@ module.exports = {
         progressive: true,
         quality: 65
       },
-      // optipng.enabled: false will disable optipng
       optipng: {
         enabled: true,
       },
